6d4cc50226d849a2d39141782579f647a5d4cecb
Imma <imacoderimacoderimacoder@gmail.com>
2024-03-23 01:36:20 -0400

    added nextsesh command and the ability to turn off the heat after a scheduled fan

--- volcanoBleServer.py
+++ volcanoBleServer.py
@@ -6,7 +6,7 @@ import sys
 from bleak import BleakClient
 
 class AsyncServer:
-    def __init__(self, turnFanOnWhenConnected, fanOnTime, host='127.0.0.1', port=65432, bt_device_address="XX:XX:XX:XX:XX:XX", initialTemp=None):
+    def __init__(self, turnFanOnWhenConnected, host='127.0.0.1', port=65432, bt_device_address="XX:XX:XX:XX:XX:XX", initialTemp=None):
         self.host = host
         self.port = port
         self.bt_device_address = bt_device_address
@@ -16,7 +16,7 @@ class AsyncServer:
         self.server_task = None
         self.fan_off_timer_task = None
         self.turnFanOnWhenConnected = turnFanOnWhenConnected
-        self.fanOnTime = fanOnTime
+        self.fanOnTime = None
 
     async def connect_bluetooth_device(self):
         def notification_handler(sender, data):
@@ -31,15 +31,12 @@ class AsyncServer:
             await self.bt_client.connect()
             await self.bt_client.start_notify("1010000c-5354-4f52-5a26-4249434b454c", notification_handler)
             if self.turnFanOnWhenConnected == True:
-                if self.fanOnTime > 0:
-                    await self.onFanOffTimer(self.fanOnTime)
-                else:
-                    await self.bt_client.write_gatt_char("10110013-5354-4f52-5a26-4249434b454c", bytes([0]))
-            if not self.initialTemp:
-                return
-            print("Writing initial temp")
-            buffer = struct.pack('<I', self.initialTemp * 10)
-            await self.bt_client.write_gatt_char("10110003-5354-4f52-5a26-4249434b454c", buffer)
+                await self.bt_client.write_gatt_char("10110013-5354-4f52-5a26-4249434b454c", bytes([0]))
+
+            if self.initialTemp:
+                print("Writing initial temp")
+                buffer = struct.pack('<I', self.initialTemp * 10)
+                await self.bt_client.write_gatt_char("10110003-5354-4f52-5a26-4249434b454c", buffer)
 
             print(f"Connected to Bluetooth device at {self.bt_device_address}")
         except Exception as e:
@@ -53,11 +50,13 @@ class AsyncServer:
             self.server_task.cancel()
             print("Server has been shut down after the delay.")
 
-    async def write_gatt_char_with_delay(self, delay, char_uuid, data):
+    async def write_gatt_char_with_delay(self, delay, char_uuid, data, turnHeatOff):
         await asyncio.sleep(delay)
         await self.bt_client.write_gatt_char(char_uuid, data)
+        if turnHeatOff:
+             await self.bt_client.write_gatt_char("10110010-5354-4f52-5a26-4249434b454c", bytes([0]))
 
-    async def onFanOffTimer(self, timeOn):
+    async def onFanOffTimer(self, timeOn, turnOffHeat):
         await self.bt_client.write_gatt_char("10110013-5354-4f52-5a26-4249434b454c", bytes([0]))
         # Cancel the existing task if it's still running
         if self.fan_off_timer_task and not self.fan_off_timer_task.done():
@@ -66,7 +65,7 @@ class AsyncServer:
         
         # Schedule the new task
         self.fan_off_timer_task = asyncio.create_task(
-            self.write_gatt_char_with_delay(timeOn, "10110014-5354-4f52-5a26-4249434b454c", bytes([0]))
+            self.write_gatt_char_with_delay(timeOn, "10110014-5354-4f52-5a26-4249434b454c", bytes([0]), turnOffHeat)
         )
     async def handle_client(self, reader, writer):
         address = writer.get_extra_info('peername')
@@ -83,12 +82,30 @@ class AsyncServer:
                 await self.bt_client.write_gatt_char("10110010-5354-4f52-5a26-4249434b454c", bytes([0]))
             elif message == "FanOn":
                 await self.bt_client.write_gatt_char("10110013-5354-4f52-5a26-4249434b454c", bytes([0]))
+            elif message == "NextSesh":
+                value = await self.bt_client.read_gatt_char("10110003-5354-4f52-5a26-4249434b454c")
+                decodedValue = value[0] + (value[1] * 256)
+                normalizedValue = round(decodedValue / 10)
+                print(normalizedValue)
+                if normalizedValue == 185:
+                    await self.bt_client.write_gatt_char("10110003-5354-4f52-5a26-4249434b454c", struct.pack('<I', 1900))
+                elif normalizedValue == 190:
+                    await self.bt_client.write_gatt_char("10110003-5354-4f52-5a26-4249434b454c", struct.pack('<I', 1950))
+                elif normalizedValue == 195:
+                    await self.bt_client.write_gatt_char("10110003-5354-4f52-5a26-4249434b454c", struct.pack('<I', 2000))
+                elif normalizedValue == 200:
+                    await self.bt_client.write_gatt_char("10110003-5354-4f52-5a26-4249434b454c", struct.pack('<I', 1850))
+                else:
+                    await self.bt_client.write_gatt_char("10110003-5354-4f52-5a26-4249434b454c", struct.pack('<I', 1850))
+
+                await self.bt_client.write_gatt_char("1011000f-5354-4f52-5a26-4249434b454c", bytes([0]))
             elif message == "FanOff":
                 await self.bt_client.write_gatt_char("10110014-5354-4f52-5a26-4249434b454c", bytes([0]))
             elif message.startswith("FanOffTimer"):
                 parts = message.split("=")
                 timeOn = float(parts[1])  # Convert the right part to integer
-                await self.onFanOffTimer(timeOn)
+                turnOffHeat = "HeatOff" in parts[0]
+                await self.onFanOffTimer(timeOn, turnOffHeat)
 
             elif message == "FanToggle":
                 fanChar = "10110013-5354-4f52-5a26-4249434b454c"
@@ -144,8 +161,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Send a message to a TCP server.')
     parser.add_argument('--initTemp', type=int, help='Message to send', default=None)
     parser.add_argument('--FanOn', type=bool, help='Turn fan on', default=False)
-    parser.add_argument('--FanOnTime', type=float, help='Optional time to keep fan on', default=0)
     parser.add_argument('--BleMacAddress', type=str, help='Mac address of your Volcano', default="XX:XX:XX:XX:XX:XX")
     args = parser.parse_args()
-    server = AsyncServer(args.FanOn, args.FanOnTime, bt_device_address=args.BleMacAddress, initialTemp=args.initTemp)  # Replace with your device's address
+    server = AsyncServer(args.FanOn, bt_device_address=args.BleMacAddress, initialTemp=args.initTemp)  # Replace with your device's address
     asyncio.run(server.run())
